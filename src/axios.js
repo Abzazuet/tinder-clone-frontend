8086ab62de61b819e23c1a3f7abe5e7a9ab25dfe
Abel Zazueta <abzazuet@amazon.com>
2022-12-26 18:53:01 -0800

    Changed the axios file to use the actual route

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
  
 const instance = axios.create({
-    baseURL: 'http://localhost:8001'
+    baseURL: 'https://ill-tan-kangaroo-cape.cyclic.app'
 });
 
 export default instance;
